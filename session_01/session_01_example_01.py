e958bafed00c99c7203539a472a4001533f89bc6
blokartwork <blok.artwork@googlemail.com>
2017-10-04 22:25:12 +0200

    Make things random

--- session_01/session_01_example_01.py
+++ session_01/session_01_example_01.py
@@ -1,9 +1,9 @@
 import rhinoscriptsyntax as rs
 import random
 
-x = 10
-y = 10
-z = 10
+x = random.uniform(-100,100)
+y = random.uniform(-100,100)
+z = random.uniform(-100,100)
 
 myPoint = (x,y,z)
 rs.AddPoint(myPoint)
