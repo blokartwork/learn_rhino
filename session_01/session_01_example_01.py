52be8a0c09a3546624a8d0ac109a7a9dd101301d
blokartwork <blok.artwork@googlemail.com>
2017-10-04 22:30:50 +0200

    Create a Loop

--- session_01/session_01_example_01.py
+++ session_01/session_01_example_01.py
@@ -1,9 +1,11 @@
 import rhinoscriptsyntax as rs
 import random
 
-x = random.uniform(-100,100)
-y = random.uniform(-100,100)
-z = random.uniform(-100,100)
-
-myPoint = (x,y,z)
-rs.AddPoint(myPoint)
+count = 0
+while count < 1000 :
+    x = random.uniform(-100,100)
+    y = random.uniform(-100,100)
+    z = random.uniform(-100,100)
+    myPoint = (x,y,z)
+    rs.AddPoint(myPoint)
+    count += 1
