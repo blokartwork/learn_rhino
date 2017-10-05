420727cf7cd8497c52db1d2819f474199b8da523
blokartwork <blok.artwork@googlemail.com>
2017-10-05 21:17:56 +0200

    Adding an if statement

--- session_01/session_01_example_01.py
+++ session_01/session_01_example_01.py
@@ -2,10 +2,12 @@ import rhinoscriptsyntax as rs
 import random
 
 count = 0
-while count < 1000 :
+while count < 1000:
     x = random.uniform(-100,100)
     y = random.uniform(-100,100)
     z = random.uniform(-100,100)
-    myPoint = (x,y,z)
-    rs.AddPoint(myPoint)
-    count += 1
+    point = (x,y,z)
+
+    if rs.Distance(point, (0,0,0)) > 90 and rs.Distance(point, (0,0,0)) < 95:
+        rs.AddPoint(point)
+        count += 1
