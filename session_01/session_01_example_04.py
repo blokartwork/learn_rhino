73737d37c5c803ee37894abc3c733667f37538f8
blokartwork <blok.artwork@googlemail.com>
2017-10-06 20:27:20 +0200

    Example 04

---
+++ session_01/session_01_example_04.py
@@ -0,0 +1,24 @@
+import rhinoscriptsyntax as rs
+import random
+import math
+
+z = -100
+radius = 0
+theta = 0
+
+while z <= 100:
+    x = radius * math.cos(theta)
+    y = radius * math.sin(theta)
+
+    if x > 30:
+        x = 30
+
+    rs.AddPoint(x,y,z)
+
+    if z > 20 and z < 80:
+        radius -= 0.03
+    else:
+        radius += 0.03
+
+    theta += 1
+    z += 0.1
