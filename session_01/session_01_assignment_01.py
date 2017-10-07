900c6fd2cced0d5918afd8303ab4a492ec9d8fd1
blokartwork <blok.artwork@googlemail.com>
2017-10-07 15:12:01 +0200

    Create Random Circles

--- session_01/session_01_assignment_01.py
+++ session_01/session_01_assignment_01.py
@@ -3,18 +3,28 @@ import random
 import math
 
 z = 0
-radius = 100
+radius = 10
 theta = 0
 pi = math.pi
+count = 0
 
 while z < 100:
     theta = 0
     while theta < (2 * pi):
-        x = radius * math.cos(theta)
-        y = radius * math.sin(theta)
+        if count % 4 == 0:
+            x = radius * math.cos(theta)
+            y = radius * math.sin(theta)
+        else:
+            x = radius * math.cos(theta)
+            y = radius * math.sin(theta)
 
+        count += 1
         rs.AddPoint(x,y,z)
-
         theta += 0.02
+
+    if z > 40 and z < 60:
+        radius += 1
+    else:
+        radius += random.uniform(0,1)
 
     z += 1
